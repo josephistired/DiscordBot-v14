8cfd8c9d78ed9751de8e0501c17041256b4c7b57
Joseph <65987360+josephistired@users.noreply.github.com>
2023-01-18 14:12:14 -0500

    Update purge.js
    fixed

--- Commands/Moderation/purge.js
+++ Commands/Moderation/purge.js
@@ -51,7 +51,7 @@ module.exports = {
       .addFields(
         {
           name: "👤 User:",
-          value: `\`\`\`${user.tag || "No specified user"}\`\`\``,
+          value: `\`\`\`${user.username || "No specified user"}\`\`\``,
         },
         {
           name: "🔘 Channel:",
