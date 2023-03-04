f183f6e45ca8b4fe66bcbf3deadf9db8ccf448f1
Joseph Carmosino <65987360+josephistired@users.noreply.github.com>
2023-03-04 07:41:57 -0500

    retrace
    .

---
+++ Commands/Systems/Welcome/welcome.js
@@ -0,0 +1,61 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("welcome")
+    .setDescription("Control the welcome system")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .addSubcommand((options) =>
+      options
+        .setName("setup")
+        .setDescription(
+          "This allows you to setup your welcome system in your server."
+        )
+        .addChannelOption((options) =>
+          options
+            .setName("welcome-channel")
+            .setDescription("Select the channel.")
+            .setRequired(true)
+        )
+        .addRoleOption((options) =>
+          options
+            .setName("welcome-role")
+            .setDescription(
+              "Provide the role you want a new member to recieve when joining."
+            )
+            .setRequired(true)
+        )
+        .addStringOption((options) =>
+          options
+            .setName("welcome-message")
+            .setDescription("Provide a custom welcome message.")
+        )
+        .addStringOption((options) =>
+          options
+            .setName("welcome-color")
+            .setDescription("Select the color of the welcome embed.")
+            .setChoices(
+              {
+                name: "red",
+                value: "Red",
+              },
+              {
+                name: "green",
+                value: "Green",
+              },
+              {
+                name: "purple",
+                value: "Purple",
+              },
+              {
+                name: "blue",
+                value: "Blue",
+              },
+              {
+                name: "white",
+                value: "White",
+              }
+            )
+        )
+    ),
+};
