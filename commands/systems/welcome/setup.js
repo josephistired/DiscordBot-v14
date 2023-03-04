9c41fcb337ceb10bddd6e9ea50ff128a087b1231
Joseph Carmosino <65987360+josephistired@users.noreply.github.com>
2023-03-04 07:35:58 -0500

    oops
    no one seen that

---
+++ commands/systems/welcome/setup.js
@@ -0,0 +1,35 @@
+const { EmbedBuilder, ChatInputCommandInteraction } = require("discord.js");
+const Database = require("../../../schemas/welcome");
+
+module.exports = {
+  subCommand: "welcome.setup",
+  /**
+   * @param {ChatInputCommandInteraction} interaction
+   *
+   */
+  async execute(interaction) {
+    const { options } = interaction;
+
+    const channelObject = options.getChannel("welcome-channel");
+    const roleObject = options.getRole("welcome-role");
+    const messageObject = options.getString("welcome-message");
+    const colorObject = options.getString("welcome-color") || "Green";
+
+    Database.findOne({ Guild: interaction.guild.id }, async (err, data) => {
+      if (!data) {
+        const newWelcome = await Database.create({
+          Guild: interaction.guild.id,
+          WelcomeChannel: channelObject.id,
+          WelcomeMessage: messageObject,
+          WelcomeColor: colorObject,
+          WelcomeRole: roleObject.id,
+        });
+      }
+    });
+
+    interaction.reply({
+      content: `The welcome system has been successfully setup.`,
+      ephemeral: true,
+    });
+  },
+};
