32013c269432cd2e30771db06398aae08a03ff01
Joseph Carmosino <65987360+josephistired@users.noreply.github.com>
2023-03-04 07:43:33 -0500

    Create welcome.js

---
+++ schemas/welcome.js
@@ -0,0 +1,12 @@
+const { model, Schema } = require("mongoose");
+
+module.exports = model(
+  "welcome",
+  new Schema({
+    Guild: String,
+    WelcomeChannel: String,
+    WelcomeMessage: String,
+    WelcomeColor: String,
+    WelcomeRole: String,
+  })
+);
