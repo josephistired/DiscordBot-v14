6d34be15e973863a20c5fc23fc6a5b43485044ca
Joseph Carmosino <65987360+josephistired@users.noreply.github.com>
2023-04-05 06:48:00 -0400

    IMPORTANT (EXPAND)
    It appears that during the update of the Command Loader file while I was testing v1.0.3, I unintentionally neglected to register the commands. I apologize for any inconvenience this may have caused and express my gratitude to the individual on Discord who brought this to my attention.

--- Handlers/commandLoader.js
+++ Handlers/commandLoader.js
@@ -8,6 +8,7 @@ async function loadCommands(client) {
   await client.subCommands.clear();
 
   const commands = [];
+  const commandsArray = [];
   const files = await loadFiles("Commands");
 
   for (const file of files) {
@@ -24,6 +25,7 @@ async function loadCommands(client) {
           Command: name,
           Status: "✅",
         });
+        commandsArray.push(command.data.toJSON());
       }
     } catch (error) {
       const commandName = file.split("/").pop().slice(0, -3);
@@ -35,6 +37,7 @@ async function loadCommands(client) {
     }
   }
 
+  client.application.commands.set(commandsArray);
   console.table(commands, ["Command", "Status", "Error"]);
   console.info(`\n\x1b[36mLoaded ${commands.length} commands.\x1b[0m`);
   console.timeEnd("Commands Loaded");
